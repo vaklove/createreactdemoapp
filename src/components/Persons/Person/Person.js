11f01b667f574089b6de8618cfb2691ad4e2102d
Vikas Shah <vikas.shah@essilorusa.com>
2018-11-05 15:59:46 -0500

    clean working copy after css loader  and yarn run eject command

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,7 +3,7 @@ import classes from "./Person.css";
 const person = props => {
   return (
     <div className={classes.Person}>
-      <p onClick={props.click}>
+      <p onClick={props.clicked}>
         I am a {props.name} and i am {props.age} years old.
       </p>
       <p>{props.children}</p>
